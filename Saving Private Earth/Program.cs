cfa8d5a6f9034af083cf1fe57987b2dcc3d878b4
Robert Schalk <RobertSchalk@users.noreply.github.com>
2019-07-25 10:06:45 -0400

    Adding default files.

---
+++ Saving Private Earth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saving_Private_Earth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
